38b9f4dd2b51dab47e88149c4fd5798c8089d27e
Ratnesh Kumar <ratneshrt19@gmail.com>
2022-07-12 09:53:11 +0530

    main.py file added

---
+++ main.py
@@ -0,0 +1,13 @@
+import phonenumbers
+from phonenumbers import geocoder
+from phonenumbers import carrier
+
+number = input("Enter your phone number with country name: ")
+
+ch_no = phonenumbers.parse(number, "CH")
+
+print(geocoder.description_for_number(ch_no, "en"))
+
+ser_no = phonenumbers.parse(number, "RO")
+
+print(carrier.name_for_number(ser_no, "en"))
